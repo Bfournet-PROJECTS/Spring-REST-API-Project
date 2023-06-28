120bf664c3d60d5167e868d716680cb7dedddec1
Benjamin FOURNET <benjamin.fournet99@gmail.com>
2023-06-28 15:47:45 +0200

    Create Dumb Record

--- src/main/java/com/ippon/project/spring_rest_api_project/main/SpringRestApiProjectApplication.java
+++ src/main/java/com/ippon/project/spring_rest_api_project/main/SpringRestApiProjectApplication.java
@@ -1,8 +1,12 @@
-package com.ippon.project.spring_rest_api_project;
+package com.ippon.project.spring_rest_api_project.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication equivalent to @Configuration, @EnableAutoConfiguration,
+ * and @ComponentScan : By default, it will scan all the components in the same package or below.
+ */
 @SpringBootApplication
 public class SpringRestApiProjectApplication {
 
