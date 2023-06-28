4138ef15816c5143c63bd95630472173be63fb17
Benjamin FOURNET <benjamin.fournet99@gmail.com>
2023-06-28 09:41:36 +0200

    First commit: Project REST API creation

---
+++ src/test/java/com/ippon/project/spring_rest_api_project/SpringRestApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ippon.project.spring_rest_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
