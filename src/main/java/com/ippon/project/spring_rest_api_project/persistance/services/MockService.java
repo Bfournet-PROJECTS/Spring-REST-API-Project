d8a75f2efb1ceb0f4bab8efd7840a0e8c84caf48
Benjamin FOURNET <benjamin.fournet99@gmail.com>
2023-06-28 16:09:32 +0200

    Mock service data creation

---
+++ src/main/java/com/ippon/project/spring_rest_api_project/persistance/services/MockService.java
@@ -0,0 +1,54 @@
+package com.ippon.project.spring_rest_api_project.persistance.services;
+
+import com.ippon.project.spring_rest_api_project.domain.Dumb;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MockService {
+
+    private final List<Dumb> dumbList = new ArrayList<>() {
+        {
+            add(new Dumb(0L, "Dumb one"));
+            add(new Dumb(1L, "Dumb two"));
+        }
+    };
+
+    public List<Dumb> findAll() {
+        return dumbList;
+    }
+
+    public Optional<Dumb> findById(Long id) {
+        return findExistingDumb(id);
+    }
+
+    public Dumb create(Dumb resource) {
+        dumbList.add(resource);
+        return resource;
+    }
+
+    public Dumb update(Long id, Dumb resource) {
+        Optional<Dumb> optionalDumb = findExistingDumb(id);
+        optionalDumb.ifPresent((dumbToUpdate) -> {
+            int index = dumbList.indexOf(dumbToUpdate);
+            dumbList.remove(dumbToUpdate);
+            dumbList.add(index, resource);
+        });
+        return resource;
+    }
+
+    public Long delete(Long id) {
+        Optional<Dumb> optionalDumb = findExistingDumb(id);
+        optionalDumb.ifPresent(dumbList::remove);
+        return id;
+    }
+
+    private Optional<Dumb> findExistingDumb(Long id) {
+        return dumbList.stream()
+                .filter(dumb -> dumb.hasId(id))
+                .findFirst();
+    }
+}
